74f5ba7895fe21ffbc8e17574d9d6e6f991430c6
Roel Kok <roel@roelkok.net>
2015-11-22 18:27:20 -0500

    Tablet media query

--- src/js/work/index.js
+++ src/js/work/index.js
@@ -2,11 +2,31 @@ var Project = require('./Project');
 
 var Work = function(root) {
 	
-	this.projects = [];
+	var projects = [];
 	
 	Array.prototype.forEach.call(root.querySelectorAll('.work__project'), function(el) {
-		this.projects.push(new Project(el));
-	}.bind(this));
+		projects.push(new Project(el));
+	});
+	
+	function updateProjects(mql) {
+		projects.forEach(function(project) {
+			if(mql.matches) {
+				project.deactivate();
+			}
+			else {
+				project.activate();
+			}
+		});
+	}
+	
+	var mql = matchMedia('(max-width: 971px)');
+	mql.addListener(function(mql) {
+		console.log('update query', mql.matches);
+		updateProjects(mql);
+	});
+	updateProjects(mql);
+	
+	
 }
 
 module.exports = Work;
